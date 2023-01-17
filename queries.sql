ed24a6ba90a9d67d49a6ff80c7278b2e88c1cb40
amexabee <amexabee@yahoo.com>
2023-01-17 20:05:59 +0200

    Update species and weight_kg

--- queries.sql
+++ queries.sql
@@ -8,5 +8,15 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true; 
 SELECT * FROM animals WHERE name NOT IN ('Gabumon'); 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
-
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE name NOT LIKE '%mon';
+COMMIT;
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT sp1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO sp1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
 
