528da63f2e9599dfc1a0de775b274e4b40b1c860
amexabee <amexabee@yahoo.com>
2023-01-17 22:57:30 +0200

    Update queries

--- queries.sql
+++ queries.sql
@@ -1,17 +1,26 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon';
+
 SELECT name FROM animals WHERE EXTRACT(year FROM date_of_birth) BETWEEN 2016 AND 2019;
+
 SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
+
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu'); 
+
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+
 SELECT * FROM animals WHERE neutered = true; 
+
 SELECT * FROM animals WHERE name NOT IN ('Gabumon'); 
+
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE name NOT LIKE '%mon';
 COMMIT;
+
 BEGIN;
 DELETE FROM animals WHERE date_of_birth > '2022-01-01';
 SAVEPOINT sp1;
@@ -20,3 +29,18 @@ ROLLBACK TO sp1;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
 
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals
+WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
+
+SELECT neutered, MIN(weight_kg) FROM animals GROUP BY neutered;
+
+SELECT neutered, MAX(weight_kg) FROM animals GROUP BY neutered;
+
+SELECT AVG(escape_attempts) FROM animals
+WHERE EXTRACT(year FROM date_of_birth) BETWEEN 1990 AND 2000;
